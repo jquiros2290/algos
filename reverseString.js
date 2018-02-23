a28d75ab8d2cc200bbd80fe20ab5d7b2174da654
Justin Quiros <jquiros2290@gmail.com>
2018-02-23 14:25:05 -0600

    Reverse String

---
+++ reverseString.js
@@ -0,0 +1,10 @@
+//reverse string
+function reverse(str){
+	var newstr = "";
+	for(var i = str.length-1; i >= 0; i--){
+		newstr+= str[i];
+    }
+	return newstr;
+}
+
+console.log(reverse("Hello"));
